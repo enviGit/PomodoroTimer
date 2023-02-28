65d28e3dcbe80814ce0604b7a491e55a5b491964
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-02-28 17:15:01 +0100

    Timer and resources added

--- PomodoroTimer/MainWindow.xaml.cs
+++ PomodoroTimer/MainWindow.xaml.cs
@@ -1,28 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PomodoroTimer.Models;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PomodoroTimer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Timer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            TimeSpan duration = TimeSpan.FromMinutes(25);
+            timer = new Timer(duration);
+            timer.Tick += Timer_Tick;
+        }
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Pause();
+        }
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Reset();
+            UpdateTimeRemainingLabel();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateTimeRemainingLabel();
+        }
+        private void UpdateTimeRemainingLabel()
+        {
+            TimeSpan remainingTime = timer.TimeRemaining;
+            Dispatcher.Invoke(() => TimeRemainingLabel.Text = remainingTime.ToString("mm\\:ss"));
         }
     }
 }
---
+++ PomodoroTimer/Models/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomodoroTimer.Models
+{
+    class Settings
+    {
+
+    }
+}
